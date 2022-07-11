f9a13dc5f717b5c8c476558d3ebb40f3ee2d1c9a
2439905184 <suchenglong018@126.com>
2022-07-11 21:39:29 +0800

    🧪 test: 添加测试代码

--- YomobPlugin/src/main/java/com/xsh/yomobplugin/YomobPlugin.java
+++ YomobPlugin/src/main/java/com/xsh/yomobplugin/YomobPlugin.java
@@ -1,9 +1,11 @@
 package com.xsh.yomobplugin;
 import android.app.Activity;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.soulgame.sgsdk.tgsdklib.TGSDK;
+import com.soulgame.sgsdk.tgsdklib.ad.ITGPreloadListener;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
@@ -13,6 +15,7 @@ public class YomobPlugin extends GodotPlugin
 {
     public Activity activity;
     public String tag;
+    public boolean isPreLoad;
     public YomobPlugin(Godot godot)
     {
         super(godot);
@@ -31,7 +34,7 @@ public class YomobPlugin extends GodotPlugin
     @UsedByGodot
     public void initSDK(String appid)
     {
-        //TGSDK.setDebugModel(true);
+        TGSDK.setDebugModel(true);
         TGSDK.initialize(activity,appid,null);
     }
    /* @UsedByGodot
@@ -42,7 +45,52 @@ public class YomobPlugin extends GodotPlugin
     @UsedByGodot
     public void showTestView(String sceneID)
     {
-        TGSDK.showTestView(activity,sceneID);
+        TGSDK.preloadAd(activity, new ITGPreloadListener() {
+            @Override
+            public void onPreloadSuccess(String s)
+            {
+                Log.d(tag,"广告预加载成功");
+                Log.d(tag,s);
+                isPreLoad = true;
+            }
+
+            @Override
+            public void onPreloadFailed(String s, String s1)
+            {
+                Log.e(tag,"广告预加载失败");
+                Log.e(s,s1);
+            }
+
+            @Override
+            public void onAwardVideoLoaded(String s)
+            {
+                Log.d(tag,"奖励视频就绪");
+                Log.d(tag,s);
+            }
+
+            @Override
+            public void onInterstitialLoaded(String s)
+            {
+                Log.d(tag,"静态插屏就绪");
+                Log.d(tag,s);
+            }
+
+            @Override
+            public void onInterstitialVideoLoaded(String s)
+            {
+                Log.d(tag,"静态插屏视频就绪");
+                Log.d(tag,s);
+            }
+        });
+        if(isPreLoad)
+        {
+            TGSDK.showTestView(activity,sceneID);
+        }
+        else
+        {
+            Log.e(tag,"广告预加载失败");
+            Log.e("场景id",sceneID);
+        }
     }
 
 }
