2b99539712467f6eebbd442ad791efae8f6bee18
2439905184 <suchenglong018@126.com>
2022-07-11 19:20:05 +0800

    init

---
+++ YomobPlugin/src/main/java/com/xsh/yomobplugin/YomobPlugin.java
@@ -0,0 +1,38 @@
+package com.xsh.yomobplugin;
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+
+import com.soulgame.sgsdk.tgsdklib.TGSDK;
+
+import org.godotengine.godot.Godot;
+import org.godotengine.godot.plugin.GodotPlugin;
+import org.godotengine.godot.plugin.SignalInfo;
+import org.godotengine.godot.plugin.UsedByGodot;
+public class YomobPlugin extends GodotPlugin
+{
+    public Activity activity;
+    public String tag;
+    public YomobPlugin(Godot godot)
+    {
+        super(godot);
+        activity = getActivity();
+        //获取到godot的根布局 并动态添加线性布局用于当做banner广告容器
+        //layout = getGodot().getFragmentManager().getFragment();
+        tag = YomobPlugin.class.toString();
+    }
+
+    @NonNull
+    @Override
+    public String getPluginName()
+    {
+        return "YomobPlugin";
+    }
+
+    @UsedByGodot
+    public void showTestView(String sceneID)
+    {
+        TGSDK.showTestView(activity,sceneID);
+    }
+
+}
